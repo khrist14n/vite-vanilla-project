b106be5bb97bb1e828de0185240ebaa0d6861347
khrist14n <khrist14n@gmail.com>
2023-01-11 21:57:03 +0000

    deploy

--- vite-vanilla-project/src/main.ts
+++ vite-vanilla-project/src/main.ts
@@ -2,9 +2,6 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import { setupCounter } from './counter'
 
-window.location.hostname = '/vite-vanilla-project/'
-
-
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
